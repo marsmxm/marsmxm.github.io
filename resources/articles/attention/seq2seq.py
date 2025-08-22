5a7790a81a26e398be934e21a9e5bb7b7ce7b925
Mu Xian Ming <marsmxm@gmail.com>
2025-08-22 10:45:54 +0800

    Update seq2seq.py

--- resources/articles/attention/seq2seq.py
+++ resources/articles/attention/seq2seq.py
@@ -58,8 +58,6 @@ def load_dataset(m):
     """
     
     dataset = []
-    Tx = 30
-    
 
     for i in tqdm(range(m)):
         h, m, _ = load_date()
@@ -71,7 +69,7 @@ def load_dataset(m):
 # %%
 m = 100000
 dataset = load_dataset(m)
-print(dataset[:5])
+dataset[:5]
 
 # %%
 import tensorflow as tf
@@ -88,51 +86,52 @@ def custom_standardization(input_string):
     # Remove all punctuation except '-'
     return tf.strings.regex_replace(lowercase, r"[^\w\s-@$]", "")
 
-data_human = [d[0] for d in dataset]
-data_machine = [d[1] for d in dataset]
+dates_human = [d[0] for d in dataset]
+dates_machine = [d[1] for d in dataset]
 
 vec_layer_human = tf.keras.layers.TextVectorization(
     vocab_size, output_sequence_length=Tx, split="character", name="vec_h", standardize=custom_standardization)
 vec_layer_machine = tf.keras.layers.TextVectorization(
     vocab_size, output_sequence_length=Ty, split="character", name="vec_m", standardize=custom_standardization)
-vec_layer_human.adapt(data_human)
-vec_layer_machine.adapt([f"{sos}{s}{eos}" for s in data_machine])
+vec_layer_human.adapt(dates_human)
+vec_layer_machine.adapt([f"{sos}{s}{eos}" for s in dates_machine])
 
-print(vec_layer_human.get_vocabulary())
+print(vec_layer_human.get_vocabulary()[:15])
 print(vec_layer_machine.get_vocabulary())
 
 # %%
-train_size = 80000
+train_size = 60000
+valid_size = 20000
 
-X_train = tf.constant(data_human[:train_size])
-X_valid = tf.constant(data_human[train_size:])
-X_train_dec = tf.constant([f"{sos}{s}" for s in data_machine[:train_size]])
-X_valid_dec = tf.constant([f"{sos}{s}" for s in data_machine[train_size:]])
-Y_train = vec_layer_machine([f"{s}{eos}" for s in data_machine[:train_size]])
-Y_valid = vec_layer_machine([f"{s}{eos}" for s in data_machine[train_size:]])
+X_train = tf.constant(dates_human[:train_size])
+X_valid = tf.constant(dates_human[train_size:train_size+valid_size])
+X_test = tf.constant(dates_human[train_size+valid_size:])
+
+X_train_dec = tf.constant([f"{sos}{s}" for s in dates_machine[:train_size]])
+X_valid_dec = tf.constant([f"{sos}{s}" for s in dates_machine[train_size:train_size+valid_size]])
+X_test_dec = tf.constant([f"{sos}{s}" for s in dates_machine[train_size+valid_size:]])
+
+Y_train = vec_layer_machine([f"{s}{eos}" for s in dates_machine[:train_size]])
+Y_valid = vec_layer_machine([f"{s}{eos}" for s in dates_machine[train_size:train_size+valid_size]])
+Y_test = vec_layer_machine([f"{s}{eos}" for s in dates_machine[train_size+valid_size:]])
+
+print(X_train[0])
+tf.cast(tf.expand_dims(vec_layer_human(X_train[0]), axis=1), dtype=tf.float32)
 
 # %%
-tf.random.set_seed(42)  # extra code – ensures reproducibility on CPU
 encoder_inputs = tf.keras.layers.Input(name="encoder_inputs", shape=[], dtype=tf.string)
 decoder_inputs = tf.keras.layers.Input(name="decoder_inputs",shape=[], dtype=tf.string)
 
-embed_size = 128
-encoder_embedding_layer = tf.keras.layers.Embedding(vocab_size, embed_size, name="enc_emb", mask_zero=True)
-decoder_embedding_layer = tf.keras.layers.Embedding(vocab_size, embed_size, name="dec_emb", mask_zero=True)
+encoder_input_ids = tf.cast(tf.expand_dims(vec_layer_human(encoder_inputs), axis=-1), dtype=tf.float32)
+decoder_input_ids = tf.cast(tf.expand_dims(vec_layer_machine(decoder_inputs), axis=-1), dtype=tf.float32)
 
-encoder_input_ids = vec_layer_human(encoder_inputs)
-decoder_input_ids = vec_layer_machine(decoder_inputs)
-encoder_embeddings = encoder_embedding_layer(encoder_input_ids)
-decoder_embeddings = decoder_embedding_layer(decoder_input_ids)
-
-# encoder = tf.keras.layers.LSTM(512, name="encoder", return_state=True)
 encoder = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_state=True), name="encoder")
-encoder_outputs, *encoder_states = encoder(encoder_embeddings)
-encoder_states = [tf.concat(encoder_states[::2], axis=-1),  # short-term (0 & 2)
-                  tf.concat(encoder_states[1::2], axis=-1)] # long-term (1 & 3)
+encoder_outputs, *encoder_states = encoder(encoder_input_ids)
+encoder_states = [tf.concat(encoder_states[::2], axis=-1),  # hidden states (0 & 2)
+                  tf.concat(encoder_states[1::2], axis=-1)] # cell states (1 & 3)
 
 decoder = tf.keras.layers.LSTM(512, name="decoder", return_sequences=True)
-decoder_outputs = decoder(decoder_embeddings, initial_state=encoder_states)
+decoder_outputs = decoder(decoder_input_ids, initial_state=encoder_states)
 
 output_layer = tf.keras.layers.Dense(vocab_size, name="dense", activation="softmax")
 Y_proba = output_layer(decoder_outputs)
@@ -142,12 +141,15 @@ model = tf.keras.Model(inputs=[encoder_inputs, decoder_inputs],
 model.compile(loss="sparse_categorical_crossentropy", optimizer="nadam",
               metrics=["accuracy"])
 
-# Show complete model summary without truncation
-model.summary(line_length=120, expand_nested=True, show_trainable=True)
+model.summary(line_length=120, expand_nested=True)
 
 # %%
 model.fit((X_train, X_train_dec), Y_train, epochs=10,
           validation_data=((X_valid, X_valid_dec), Y_valid))
+
+print("Evaluate on test data")
+results = model.evaluate((X_test, X_test_dec), Y_test)
+print("test loss, test acc:", results)
 
 # %%
 import numpy as np
@@ -169,5 +171,4 @@ def translate(hunman_date):
 for i in range(10):
     human_date= format_date(fake.date_object(), format=random.choice(FORMATS),  locale='en_US')
     print("human: " + human_date)
-    print("machine: " + translate(human_date))
-    print("=" * 12)
+    print("machine: " + translate(human_date) + "\n")
